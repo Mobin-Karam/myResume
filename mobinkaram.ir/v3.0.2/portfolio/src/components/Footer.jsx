6a750876a65b7cc40050443dc9f4dc5840c5ad1a
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-06-08 14:04:49 +0330

    work

--- mobinkaram.ir/v3.0.2/portfolio/src/components/Footer.jsx
+++ mobinkaram.ir/v3.0.2/portfolio/src/components/Footer.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 import avatar from "../img/logo.png";
 import { FaBeer, FaBlenderPhone } from "react-icons/fa";
-import { BsGithub } from "react-icons/bs";
+import { RiGitRepositoryLine } from "react-icons/ri";
+import {
+  BsFillFileTextFill,
+  BsGithub,
+  BsJournalRichtext,
+} from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  // const [user, setUser] = useState([]);
-
   const user = {
     name: "Mobin Karam",
     avatar_url: `${avatar}`,
     bio: "This is Mobin Karam from his Setup and his just learn learn and learn about his skills ",
+    company: "My StartUp",
+    public_repos: 8,
+    following: 0,
+    followers: 0,
   };
+
+  // const [user, setUser] = useState([]);
 
   // useEffect(() => {
   //   const fetchData = async () => {
   //     try {
-  //       // const res = await axios.get("https://api.github.com/users/Mobin-Karam");
+  //       const res = await axios.octokit.request("https://api.github.com/users/Mobin-Karam");
   //       setUser(res.data);
   //       console.log(res.data);
   //     } catch (err) {
@@ -25,26 +34,42 @@ const Footer = () => {
   //   };
   //   fetchData();
   // }, []);
+
   return (
     <>
       <div className="w-full bg-slate-500 flex items-start justify-start flex-row flex-nowrap ">
         <Link
-          className="w-400px  m-40px"
+          className="  m-40px"
           to={"https://github.com/Mobin-Karam"}
           target="_blank"
         >
-          <div className="w-400px h-200px p-10px bg-slate-400  rounded-xl flex items-start justify-start flex-col flex-nowrap ">
-            <div className="w-full flex items-center justify-center font-bold text-xl">
+          <div className="p-20px bg-slate-900/60 hover:bg-slate-950 group text-white  rounded-xl flex items-start justify-start flex-col flex-nowrap transition-all duration-300">
+            <div className="w-full flex items-center justify-center font-bold text-xl border-transparent border-b-2  group-hover:border-b-2  group-hover:border-white pb-10px mb-20px transition-all duration-300">
               <BsGithub className="text-3xl" />
               <h1 className="ml-10px"> GitHub</h1>
             </div>
             <div className="w-full flex items-center">
-              <img
-                src={user.avatar_url}
-                alt=""
-                className="w-60px h-60px rounded-full bg-slate-200 p-4px"
-              />
-              <span className="">{user.name}</span>
+              <div className="m-10px">
+                <img
+                  src={user.avatar_url}
+                  alt=""
+                  className="w-100px h-100px rounded-full bg-slate-200 p-4px"
+                />
+              </div>
+              <div className="flex items-start justify-start flex-col flex-nowrap">
+                <span className="text-2xl font-bold mb-10px">{user.name}</span>
+                <div className="flex items-start justify-start flex-row flex-nowrap">
+                  <p className="mr-10px">Followers</p>
+                  <span className=""> {user.followers}</span>
+                  <p className="mr-10px ml-10px">Following</p>
+                  <span className=""> {user.following}</span>
+                </div>
+                <div className="flex items-center  justify-start flex-row flex-nowrap mt-10px hover:bg-slate-700 p-10px rounded-lg">
+                  <p className="">PublicRepositories</p>
+                  <RiGitRepositoryLine className="ml-10px mr-10px" />
+                  <span className="">{user.public_repos}</span>
+                </div>
+              </div>
             </div>
             <div className=""></div>
           </div>
--- mobinkaram.ir/v3.0.2/portfolio/src/pages/About.jsx
+++ mobinkaram.ir/v3.0.2/portfolio/src/pages/About.jsx
@@ -4,96 +4,11 @@ import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 const About = () => {
-  const [country, setCountry] = useState([]);
-  // const [value, setValue] = useState(state?.username || "");
-  // const username = document.getElementById("username");
-  // Octokit.js
-  // https://github.com/octokit/core.js#readme
-
-  // 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
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // const res = await axios.get("https://api.github.com/users/Mobin-Karam");
-        const res = await axios.get("https://restcountries.com/v3.1/all");
-        setCountry(res.data);
-        console.log(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-
-  // avatar_url: "https://avatars.githubusercontent.com/u/124531134?v=4";
-  // bio: "This is Mobin Karam from his Setup and his just learn learn and learn about his skills ";
-  // blog: "mobinkaram.ir";
-  // company: "My StartUp";
-  // created_at: "2023-02-05T20:45:31Z";
-  // email: null;
-  // events_url: "https://api.github.com/users/Mobin-Karam/events{/privacy}";
-  // followers: 0;
-  // followers_url: "https://api.github.com/users/Mobin-Karam/followers";
-  // following: 0;
-  // following_url: "https://api.github.com/users/Mobin-Karam/following{/other_user}";
-  // gists_url: "https://api.github.com/users/Mobin-Karam/gists{/gist_id}";
-  // gravatar_id: "";
-  // hireable: null;
-  // html_url: "https://github.com/Mobin-Karam";
-  // id: 124531134;
-  // location: "iran/kermanshah";
-  // login: "Mobin-Karam";
-  // name: "Mobin Karam";
-  // node_id: "U_kgDOB2wxvg";
-  // organizations_url: "https://api.github.com/users/Mobin-Karam/orgs";
-  // public_gists: 0;
-  // public_repos: 8;
-  // received_events_url: "https://api.github.com/users/Mobin-Karam/received_events";
-  // repos_url: "https://api.github.com/users/Mobin-Karam/repos";
-  // site_admin: false;
-  // starred_url: "https://api.github.com/users/Mobin-Karam/starred{/owner}{/repo}";
-  // subscriptions_url: "https://api.github.com/users/Mobin-Karam/subscriptions";
-  // twitter_username: null;
-  // type: "User";
-  // updated_at: "2023-05-31T05:57:47Z";
-  // url: "https://api.github.com/users/Mobin-Karam";
-
   return (
     <>
       <Helmet>
         <title>Mobin Karam | About</title>
       </Helmet>
-      {/* <div className="about" id="about">
-        <input
-          className="w-500px h-50px p-10px text-white bg-slate-500 mb-10px"
-          type="search"
-          name="username"
-          id="username"
-          placeholder="Username"
-        />
-        <div className="w-500px h-400px p-10px bg-slate-500 flex items-center justify-between flex-col flex-nowrap">
-          <div className="w-full flex items-start justify-between flex-row flex-nowrap">
-            <div className="w-20% h-100px ">
-              <img
-                className="w-80px h-80px object-cover "
-                src={user.avatar_url}
-                alt=""
-              />
-            </div>
-            <div className="">{user.name}</div>
-          </div>
-          <div className="">{user.bio}</div>
-          <div className="">{user.public_repos}</div>
-          <div className="flex items-center justify-between flex-row flex-nowrap">
-            <div className="bg-slate-800 rounded-full px-16px py-5px text-lg inline-block text-white mr-10px">
-              Followers : {user.followers}
-            </div>
-            <div className="bg-slate-800 rounded-full px-16px py-5px text-lg inline-block text-white ">
-              Following : {user.following}
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
