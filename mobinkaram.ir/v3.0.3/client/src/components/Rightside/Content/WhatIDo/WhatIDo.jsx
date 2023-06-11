7e08bea76345f953953f18507ec1eddf1d2dcfa5
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-06-11 17:58:41 +0330

    start whatido section and leave it for just now

--- mobinkaram.ir/v3.0.3/client/src/components/Rightside/Content/WhatIDo/WhatIDo.jsx
+++ mobinkaram.ir/v3.0.3/client/src/components/Rightside/Content/WhatIDo/WhatIDo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const WhatIDo = () => {
-  return <div className="p-10px bg-slate-500">WhatIDo</div>;
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center flex-col flex-nowrap py-40px bg-slate-300 ">
+        <div className="relative w-full flex items-center justify-center flex-row flex-nowrap">
+          <span className="text-9xl font-bold text-gray-900/5 font-popp">
+            SERVICES
+          </span>
+          <span className="border-b-2 text-4xl font-popp font-semibold border-sky-500 absolute top-100">
+            What I Do?
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WhatIDo;
