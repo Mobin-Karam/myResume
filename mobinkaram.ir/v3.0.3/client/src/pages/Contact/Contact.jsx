b86bc9cbe84f6cac9e2096d1871cd823a9f57855
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-06-12 22:27:20 +0330

    make server side of mobinkaram.ir

--- mobinkaram.ir/v3.0.3/client/src/pages/Contact/Contact.jsx
+++ mobinkaram.ir/v3.0.3/client/src/pages/Contact/Contact.jsx
@@ -1,8 +1,48 @@
 //Contact Component
-import { FaPhone } from "@react-icons/all-files/fa/FaPhone";
+import {} from "react-icons/ai";
+import {} from "react-icons/bi";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import {} from "react-icons/cg";
+import {} from "react-icons/ci";
+import {} from "react-icons/di";
+import { FaPhone } from "react-icons/fa";
+import {} from "react-icons/fc";
+import {} from "react-icons/gi";
+import {} from "react-icons/fi";
+import { GoBookmark } from "react-icons/go";
+import {} from "react-icons/gr";
+import { HiOutlineMail } from "react-icons/hi";
+import {} from "react-icons/hi2";
+import {} from "react-icons/im";
+import {} from "react-icons/io";
+import {} from "react-icons/io5";
+import {} from "react-icons/lu";
+import {} from "react-icons/lib";
+import {} from "react-icons/md";
+import {} from "react-icons/ri";
+import {} from "react-icons/rx";
+import {} from "react-icons/si";
+import {} from "react-icons/sl";
+import {} from "react-icons/tb";
+import {} from "react-icons/tfi";
+import {} from "react-icons/ti";
+import {} from "react-icons/vsc";
 import React from "react";
 
 const Contact = () => {
+  const [formStatus, setFormStatus] = React.useState("Send");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormStatus("Submitting...");
+    const { name, email, message } = e.target.elements;
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    console.log(conFom);
+  };
+
   return (
     <>
       <div className="w-full h-screen py-50px bg-slate-300 font-popp">
@@ -27,16 +67,38 @@ const Contact = () => {
                 </div>
                 <div className="">
                   <div className="">
-                    <FaPhone /> 
+                    <FaPhone className="text-sky-500 text-xl" />
                   </div>
-                  <div className=""></div>
-                  <div className=""></div>
+                  <div className="">
+                    <HiOutlineMail className="text-sky-500 text-xl" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-start-2 col-end-6">
-            <div className=""></div>
+          <div className="col-start-2 col-end-6 pl-150px">
+            <div className="">
+              <h2 className="font-popp text-3xl pb-20px">SEND US A NOTE</h2>
+              <div className="">
+                <form action="" className="">
+                  <div className="">
+                    <div className="">
+                      <input type="text" name="contact_name" className="" />
+                      <input type="email" name="contact_email" className="" />
+                    </div>
+                    <div className="">
+                      <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        className=""
+                      ></textarea>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
