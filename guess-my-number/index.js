7d8826438e76a48004ae885936c303dde8c13918
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-11 23:56:26 +0330

    commit

---
+++ guess-my-number/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+document.querySelector('.message').textContent = '🎉Correct Number!';
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 17;
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value)
+  
