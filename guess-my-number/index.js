fcd9db438c507b0cbcf03024d93f7ca26d2a43a1
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-13 23:00:42 +0330

    first ofgame

--- guess-my-number/index.js
+++ guess-my-number/index.js
@@ -1,8 +1,29 @@
+//! use mode strict 
 'use strict'
 
-document.querySelector('.message').textContent = '🎉Correct Number!';
-document.querySelector('.number').textContent = 15;
-document.querySelector('.score').textContent = 17;
-document.querySelector('.guess').value = 10;
-console.log(document.querySelector('.guess').value)
-  
+
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+// document.querySelector('.number').textContent = 15;
+// document.querySelector('.score').textContent = 17;
+// document.querySelector('.guess').value = 10;
+// console.log(document.querySelector('.guess').value)
+
+const number = Math.trunc(Math.random() * 20 + 1);
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '🛑 هیچ عددی وجود ندارد!';
+    } else {
+        if (number === guess) {
+            document.querySelector('.message').textContent = '🎉 Correct Number!';
+            document.querySelector('.number').textContent = `${number}`;
+        } else {
+            document.querySelector('.message').textContent = `❌ Wrong Number!`;
+        }
+    }
+});
+
