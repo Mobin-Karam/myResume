c34665dcaf4b93239d69e1105a25f433c303f259
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-18 20:35:00 +0330

    start js file project ModalWindow

---
+++ PracticeProject/modalwindow/script.js
@@ -0,0 +1,9 @@
+'use strict'
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal)
+
+for (let i = 0; i < btnOpenModal.length; i++) console.log(btnOpenModal[i])
