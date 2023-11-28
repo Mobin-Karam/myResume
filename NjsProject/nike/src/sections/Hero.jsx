96751241e4b4c923a47f2d43c8cf13b3f22a1b3d
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 19:34:14 +0330

    fix url for show img pic in screen

--- NjsProject/nike/src/sections/Hero.jsx
+++ NjsProject/nike/src/sections/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
       <div className=" relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoeImg}
+          src={`.${bigShoeImg}`}
           alt="shoe collection"
           width={610}
           height={500}
@@ -55,7 +55,7 @@ const Hero = () => {
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
-                imgURL={shoe}
+                imgURL={`.${shoe}`}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
               />
