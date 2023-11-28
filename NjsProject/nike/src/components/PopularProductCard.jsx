051922d346bb2a3c6bb62d320dd4ad148dab2f23
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 20:13:35 +0330

    another fix url

--- NjsProject/nike/src/components/PopularProductCard.jsx
+++ NjsProject/nike/src/components/PopularProductCard.jsx
@@ -5,7 +5,7 @@ const PopularProductCard = ({ imgURL, name, price }) => {
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={`.${imgURL}`} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-stretch gap-2.5">
-        <img src={`.${star}`} alt="rating" width={24} height={24} />
+        <img src={star} alt="rating" width={24} height={24} />
         <p className="font-montserrat text-slate-gray text-xl leading-normal">(4.5)</p>
       </div>
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
