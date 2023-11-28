782a42db7794b5b252e7fc4d16f4dcb2529cfcf0
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 18:33:32 +0330

    make file SpecialOffer.jsx

---
+++ NjsProject/nike/src/sections/SpecialOffer.jsx
@@ -0,0 +1,43 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
+const SpecialOffer = () => {
+  return (
+    <section className="flex items-center max-xl:flex-col-reverse gap-10 max-container ">
+      <div className="flex-1">
+        <img
+          src={offer}
+          width={773}
+          height={687}
+          alt=""
+          className="object-contain w-full"
+        />
+      </div>{" "}
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Sepcial</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a tound of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4 ">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button
+            label="Learn More"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialOffer;
