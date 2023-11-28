a9e3a35702b5140f33c726afecf830a20556846b
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 19:34:46 +0330

    fix url for show img pic

--- NjsProject/nike/src/components/PopularProductCard.jsx
+++ NjsProject/nike/src/components/PopularProductCard.jsx
@@ -3,9 +3,9 @@ import {star} from '../assets/icons'
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <img src={`.${imgURL}`} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-stretch gap-2.5">
-        <img src={star} alt="rating" width={24} height={24} />
+        <img src={`.${star}`} alt="rating" width={24} height={24} />
         <p className="font-montserrat text-slate-gray text-xl leading-normal">(4.5)</p>
       </div>
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
