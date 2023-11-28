4b01bec14e75ec7b5b384f955025738fa05ec48d
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 18:29:40 +0330

    index.js of sections

---
+++ NjsProject/nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe
+}
