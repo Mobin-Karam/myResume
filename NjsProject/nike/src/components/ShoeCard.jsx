9d5666b5b2b9dcb76807e6397078b232ba0b2366
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-28 19:37:43 +0330

    fix url for show img

--- NjsProject/nike/src/components/ShoeCard.jsx
+++ NjsProject/nike/src/components/ShoeCard.jsx
@@ -17,7 +17,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
       <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img
           className="object-contain"
-          src={imgURL.thumbnail}
+          src={`.${imgURL.thumbnail}`}
           alt="shoe collection"
           width={127}
           height={103}
