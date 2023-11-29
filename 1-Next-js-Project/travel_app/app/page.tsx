302ea8848b9ffaa53f3283bf37922be708ecc5a8
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-29 16:32:51 +0330

    add footer component

--- 1-Next-js-Project/travel_app/app/page.tsx
+++ 1-Next-js-Project/travel_app/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-   <h1 className='font-bold text-teal-500'>
-    Hello world!
-   </h1>
-  )
+  return <h1 className="font-bold text-teal-500">Hello world!</h1>;
 }
