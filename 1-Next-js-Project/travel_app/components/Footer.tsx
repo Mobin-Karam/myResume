db99bc345ef7145762336e2dbcc0b69f9bd11bdc
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-30 00:36:57 +0330

    Footer Done!.

--- 1-Next-js-Project/travel_app/components/Footer.tsx
+++ 1-Next-js-Project/travel_app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_LINKS } from "@/constants";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -13,9 +13,9 @@ const Footer = () => {
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title} >
+              <FooterColumn title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link)=>(
+                  {columns.links.map((link) => (
                     <Link href="/" key={link}>
                       {link}
                     </Link>
@@ -23,8 +23,39 @@ const Footer = () => {
                 </ul>
               </FooterColumn>
             ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link href="/" className="flex gap-4 md:flex-col lg:flex-row">
+                    <p className="whitespace-nowrap">{link.label}:</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => (
+                    <Link href="/" key={link}>
+                      <Image src={link} alt="logo" width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
         </div>
+
+        <div className="border bg-gray-20 "></div>
+        <p className="regular-14 w-full text-center text-gray-30">
+          {" "}
+          2023 Hilink | All rights reserved
+        </p>
       </div>
     </footer>
   );
@@ -32,8 +63,8 @@ const Footer = () => {
 
 type FooterColumnProps = {
   title: string;
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
