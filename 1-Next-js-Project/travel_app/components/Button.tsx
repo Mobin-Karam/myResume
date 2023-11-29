a4bec42276ae8857328d9586b5d69564a7a8b727
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-11-30 00:12:13 +0330

    add some code !.

--- 1-Next-js-Project/travel_app/components/Button.tsx
+++ 1-Next-js-Project/travel_app/components/Button.tsx
@@ -6,15 +6,16 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 // The most important things is props in Button Components
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'} `}
       type={type}
     >
-      <label className="bold-16 whitespace-nowrap">{title}</label>{" "}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>{" "}
       {icon && (
         <Image className="" src={icon} alt={title} width={24} height={24} />
       )}
--- 1-Next-js-Project/travel_app/components/Footer.tsx
+++ 1-Next-js-Project/travel_app/components/Footer.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
-
+import { FOOTER_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-fll flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title} >
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link)=>(
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode
 }
 
-export default Footer
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
