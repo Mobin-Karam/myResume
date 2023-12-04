f7736b624554edda52671c242f258f518687e02b
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-04 23:03:25 +0330

    last changes

--- JavaScript/JS-exersice-questions/src/script.js
+++ JavaScript/JS-exersice-questions/src/script.js
@@ -14,22 +14,23 @@
 // console.log(newDate)
 // console.log(date)
 
+//! The Answer 
 
-const newDate = new Date();
-const date = document.querySelector('.date');
-const time = document.querySelector('.time');
-const dayOfWeek = {
-    0: "Saturday",
-    1: "Sunday",
-    2: "Monday",
-    3: "Tuesday",
-    4: "Wendsday",
-    5: "Thursday",
-    6: "Friday",
-}
+// const newDate = new Date();
+// const date = document.querySelector('.date');
+// const time = document.querySelector('.time');
+// const dayOfWeek = {
+//     0: "Saturday",
+//     1: "Sunday",
+//     2: "Monday",
+//     3: "Tuesday",
+//     4: "Wendsday",
+//     5: "Thursday",
+//     6: "Friday",
+// }
 
-date.textContent = ` ${newDate.getFullYear()}.${newDate.getMonth()}.${newDate.getDate()} `
-time.textContent = ` ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} | ${dayOfWeek[newDate.getDay()]} `
+// date.textContent = ` ${newDate.getFullYear()}.${newDate.getMonth()}.${newDate.getDate()} `
+// time.textContent = ` ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} | ${dayOfWeek[newDate.getDay()]} `
 
 // console.log(date)
 // console.log(dayOfWeek[newDate.getDay()])
