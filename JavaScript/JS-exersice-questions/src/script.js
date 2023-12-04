26f7d5ff71d803e81c23721dfa0c1b73da0a36c5
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-04 20:40:33 +0330

    start js exersices questions

---
+++ JavaScript/JS-exersice-questions/src/script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//^ #1 Question
+//* About Date and Time
+
+//* stampTime 01 Jan 1970
+// const newDate = new Date(2*365*24*3600*1000);
+// let date = document.querySelector('.date');
+
+//* Date before 01.01.1970 have negative timestamp 
+
+// date.textContent = `${newDate}`
+
+// console.log(newDate)
+// console.log(date)
+
+
+const newDate = new Date();
+const 
+
+console.log(newDate.getFullYear(),newDate.getMonth(),newDate.getDate())
+
+
+
+
+
+//^ #2 
+
+
+
