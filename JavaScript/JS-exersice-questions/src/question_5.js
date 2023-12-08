d9be25b3282cf7e4a89ff3a03b5ba03670f83fe2
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-08 21:02:38 +0330

    add questions 5

---
+++ JavaScript/JS-exersice-questions/src/question_5.js
@@ -0,0 +1,20 @@
+'use strict'
+
+//^ #5 
+
+let word = 'w3resource';
+let count = 0;
+// for (let i = 0; i < word.length; i++)
+// console.log(`${word[i]}`)
+// if (word[i] === 'r') {
+//     count++;
+// }
+// console.log(word[i])
+// let reWord = `${word[9]}${word[8]}${word[7]}${word[6]}${word[5]}${word[4]}${word[3]}${word[2]}${word[1]}${word[0]}`
+// console.log(reWord)
+
+// console.log(word.length)
+
+for (let number of '123456') {
+    console.log(number)
+}
