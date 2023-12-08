340b4f378e526b2f71634783f077e7281eca4ef8
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-08 16:32:35 +0330

    make file for question4 and Done!

---
+++ JavaScript/JS-exersice-questions/src/question_4.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//^ #4
+// 4. Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.  
+
+//* Tip :: A = √[s(s - a)(s - b)(s - c)], where 's' is the semi - perimeter of the triangle given by s = (a + b + c) / 2.
+
+
+const areaT = (a, b, c) => {
+    let s = (a + b + c) / 2;
+    let A = s * (s - a) * (s - b) * (s - c);
+    return (A)
+};
+console.log(Math.sqrt(areaT(5, 6, 7)))
+console.log(Math.cbrt)
