d493bfb204d977fe9756fb5361df602a2a3d47bd
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-08 09:51:52 +0330

    set timer that refresh time in every seconds

--- JavaScript/JS-exersice-questions/src/script.js
+++ JavaScript/JS-exersice-questions/src/script.js
@@ -16,28 +16,92 @@
 
 //! The Answer 
 
-// const newDate = new Date();
-// const date = document.querySelector('.date');
-// const time = document.querySelector('.time');
-// const dayOfWeek = {
-//     0: "Saturday",
-//     1: "Sunday",
-//     2: "Monday",
-//     3: "Tuesday",
-//     4: "Wendsday",
-//     5: "Thursday",
-//     6: "Friday",
+
+const newDate = new Date();
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
+const daysOfWeek = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wendsday",
+    "Thursday",
+    "Friday",
+]
+
+date.textContent = ` ${newDate.getFullYear()}.${newDate.getMonth()}.${newDate.getDate()} `
+time.textContent = ` ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} | ${daysOfWeek[newDate.getDay()]} `
+
+// Make time refresh in every Seconds
+let timer = () => {
+    // Get newDate Every time
+    const newDate = new Date();
+    let hours = newDate.getHours()
+    let minutes = newDate.getMinutes()
+    let seconds = newDate.getSeconds()
+    let daysOfWeeks = daysOfWeek[newDate.getDay()]
+    // Set QuerySeletor
+    let time = document.querySelector('.time');
+    // Set time Layout
+    time.textContent = ` ${hours}:${minutes}:${seconds} | ${daysOfWeeks} `
+    // Show in Console.log()
+    // console.log(time)
+    // console.log(newDate)
+}
+
+
+
+setInterval(timer, 1000);
+
+
+// let timerId = setTimeout(function tick() {
+//     const newDate = new Date()
+//     document.querySelector('.seconds').textContent = `${newDate.getSeconds}`
+//     timerId = setTimeout(tick, 1000); // (*)
+// }, 1000);
+
+// let timer = function () {
+// const time = document.querySelector('.time')
+//     return (
+//         time.textContent = ` ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} | ${dayOfWeek[newDate.getDay()]} `
+//     )
 // }
 
-// date.textContent = ` ${newDate.getFullYear()}.${newDate.getMonth()}.${newDate.getDate()} `
-// time.textContent = ` ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()} | ${dayOfWeek[newDate.getDay()]} `
+// setInterval(() => {
+//     timer()
+// }, 1000);
 
-// console.log(date)
-// console.log(dayOfWeek[newDate.getDay()])
+console.log(date)
+console.log(daysOfWeek[newDate.getDay()])
 
 
 
 
+// setTimeout(function () { window.location.reload(); }, 1000);
+// let timerId = setInterval(() => window.location.reload(), 1000);
+
+// function sayHi() {
+//     alert('Hi Mobin')
+// }
+
+// setTimeout(sayHi,3000)
+
+// function sayHi(pharse, who) {
+//     return alert(pharse + ', ' + who)
+// }
+
+// let timerId = setTimeout((pharse, who) => { alert(pharse + ', ' + who) }, 2000, 'Hello', 'Mobin')
+
+// clearTimeout(timerId);
+
+// alert(timerId + ' Done!')
+
+
+// let timerId = setTimeout(function tick() {
+//     alert('tick');
+//     timerId = setTimeout(tick, 2000); // (*)
+// }, 2000);
 
 //^ #2 
 
