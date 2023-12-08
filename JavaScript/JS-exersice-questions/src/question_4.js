d97c980a1474de0357199e9d56a5ab8a86e84985
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-08 21:02:10 +0330

    REMOVE some extra code

--- JavaScript/JS-exersice-questions/src/question_4.js
+++ JavaScript/JS-exersice-questions/src/question_4.js
@@ -5,11 +5,13 @@
 
 //* Tip :: A = √[s(s - a)(s - b)(s - c)], where 's' is the semi - perimeter of the triangle given by s = (a + b + c) / 2.
 
-
+// Calculation the area of triangle
 const areaT = (a, b, c) => {
     let s = (a + b + c) / 2;
     let A = s * (s - a) * (s - b) * (s - c);
     return (A)
 };
+
+// console.log() The answer of question
 console.log(Math.sqrt(areaT(5, 6, 7)))
-console.log(Math.cbrt)
+
