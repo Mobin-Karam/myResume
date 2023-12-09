da29d7b32615c67f991c9267527e5f26b561c72c
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-09 11:32:13 +0330

    start build DevFlowApp

--- 1-Njs-Project/devflowapp/app/page.tsx
+++ 1-Njs-Project/devflowapp/app/page.tsx
@@ -1,10 +1,12 @@
-
+// Home Page
 
 export default function Home() {
   return (
 
       <div className="">
-        <h1 className="">Hello My Next.js Project 13</h1>
+        <h1 className="h1-bold">Hello My Next.js Project 13</h1>
+        <h2 className="h2-bold">Hello My Next.js Project 13</h2>
+        <h3 className="h3-bold">Hello My Next.js Project 13</h3>
       </div>
 
   );
