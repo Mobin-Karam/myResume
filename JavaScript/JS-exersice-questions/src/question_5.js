a60cd46ac1b8b500b5fc3923aeca1e64039286ce
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-12 23:57:45 +0330

    question5 js file

--- JavaScript/JS-exersice-questions/src/question_5.js
+++ JavaScript/JS-exersice-questions/src/question_5.js
@@ -2,19 +2,7 @@
 
 //^ #5 
 
-let word = 'w3resource';
-let count = 0;
-// for (let i = 0; i < word.length; i++)
-// console.log(`${word[i]}`)
-// if (word[i] === 'r') {
-//     count++;
-// }
-// console.log(word[i])
-// let reWord = `${word[9]}${word[8]}${word[7]}${word[6]}${word[5]}${word[4]}${word[3]}${word[2]}${word[1]}${word[0]}`
-// console.log(reWord)
+let text = document.querySelector('.Text').textContent = 'w3source';
 
-// console.log(word.length)
 
-for (let number of '123456') {
-    console.log(number)
-}
+console.log(text[7],text[6],text[5],text[4],text[3],text[2],text[1],text[0])
