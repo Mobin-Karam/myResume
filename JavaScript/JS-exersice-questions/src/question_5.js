548422884d3bc294e9eb765466aac337210c42bf
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-13 23:38:26 +0330

    add more if else

--- JavaScript/JS-exersice-questions/src/question_5.js
+++ JavaScript/JS-exersice-questions/src/question_5.js
@@ -11,13 +11,35 @@ const firstName = prompt('What is your First Name ?')
 const lastName = prompt('What is your Last Name ?')
 const getBirthYear = Number(prompt('When you born ?'));
 
+const checkNameLength = (firstName.length >= 3, firstName.length <= 20, lastName.length <= 20, lastName.length >= 3)
+
 let currentYear = getSina.getFullYear();
 
 let age = currentYear - getBirthYear;
 
-if (firstName.length > 3) {
-    text.innerHTML += `<h2>Hello ${firstName} ${lastName}.</h2>`;
+if (firstName !== "") {
+    if (firstName.length >= 3, firstName.length <= 20) {
+        console.log('FirstName be Checked ✅')
+        if (lastName.length >= 3, lastName.length <= 20) {
+            console.log('LastName be Checked ✅')
+        }
+    }
+} else if (firstName == "") {
+    alert('Hello \nPlease insert Your FirstName in input \n      Thanks you. 🎉')
 }
+
+// if (checkNameLength) {
+//         alert('FirstName & LastName be Checked ✅')
+//         if (age >= authAge) {
+//             text.innerHTML += `Hello Mr./Mrs ${firstName} ${lastName}. You are ${age} Years Old Today and you can take driver's license ✅`;
+//         } else {
+//             text.textContent = `Your age is ${age} and Your Full name is ${firstName} ${lastName} and you can't take driver's license 🛑`;
+//         }
+//     } else {
+//         alert('FirstName & LastName be Checked Please Enter your Name Above or Equal to 3 Characters 🛑')
+//     }
+
+//==========================================
 
 // if (authAge <= (age)) {
 //     text.textContent = `Your age is ${age} and Your Full name is ${firstName} ${lastName} and you can take driver's license ✅`;
