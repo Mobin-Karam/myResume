100396e28e54feae9ffe64ecaa1da093e586835f
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-13 23:59:06 +0330

    add input to field, make button handler

--- JavaScript/JS-exersice-questions/src/question_5.js
+++ JavaScript/JS-exersice-questions/src/question_5.js
@@ -2,32 +2,45 @@
 
 //^ #5 
 
+// Get New Date
 let getSina = new Date();
 
+// Place that end text complete is be Shown.
 const text = document.getElementById('Text')
 
+// AuthAge
 const authAge = 18;
-const firstName = prompt('What is your First Name ?')
-const lastName = prompt('What is your Last Name ?')
-const getBirthYear = Number(prompt('When you born ?'));
 
-const checkNameLength = (firstName.length >= 3, firstName.length <= 20, lastName.length <= 20, lastName.length >= 3)
+// FirstName
+const firstName = document.getElementById('inputText')
+console.log(firstName.value)
 
-let currentYear = getSina.getFullYear();
+const checkedButton = () => {
+    const fnInputValue = firstName.value
+    console.log(fnInputValue)
+};
 
-let age = currentYear - getBirthYear;
+// const firstName = prompt('What is your First Name ?')
+// const lastName = prompt('What is your Last Name ?')
+// const getBirthYear = Number(prompt('When you born ?'));
+//============================================= # 1-2 #######
+// const checkNameLength = (firstName.length >= 3, firstName.length <= 20, lastName.length <= 20, lastName.length >= 3)
 
-if (firstName !== "") {
-    if (firstName.length >= 3, firstName.length <= 20) {
-        console.log('FirstName be Checked ✅')
-        if (lastName.length >= 3, lastName.length <= 20) {
-            console.log('LastName be Checked ✅')
-        }
-    }
-} else if (firstName == "") {
-    alert('Hello \nPlease insert Your FirstName in input \n      Thanks you. 🎉')
-}
+// let currentYear = getSina.getFullYear();
 
+// let age = currentYear - getBirthYear;
+
+// if (firstName !== "") {
+//     if (firstName.length >= 3, firstName.length <= 20) {
+//         console.log('FirstName be Checked ✅')
+//         if (lastName.length >= 3, lastName.length <= 20) {
+//             console.log('LastName be Checked ✅')
+//         }
+//     }
+// } else if (firstName == "") {
+//     alert('Hello \nPlease insert Your FirstName in input \n      Thanks you. 🎉')
+// }
+///============================================== # 1-1 #######
 // if (checkNameLength) {
 //         alert('FirstName & LastName be Checked ✅')
 //         if (age >= authAge) {
@@ -40,10 +53,3 @@ if (firstName !== "") {
 //     }
 
 //==========================================
-
-// if (authAge <= (age)) {
-//     text.textContent = `Your age is ${age} and Your Full name is ${firstName} ${lastName} and you can take driver's license ✅`;
-// } else {
-
-//     text.textContent = `Your age is ${age} and Your Full name is ${firstName} ${lastName} and you can't take driver's license 🛑`;
-// } 
