61367e2d9b6e8e2d12f742b4c6fb1ade9149fc06
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-16 23:04:17 +0330

    .

--- JavaScript/JS-exersice-questions/src/question_5.js
+++ JavaScript/JS-exersice-questions/src/question_5.js
@@ -2,28 +2,3 @@
 
 //^ #5 
 
-
-
-// Define a function named animate_string that takes an id as a parameter
-const animate_string = (id) => {
-    // Get the element with the specified id
-    const element = document.getElementById(id);
-
-    // Get the text node from the first child of the element
-    const textNode = element.childNodes[0]; // assuming no other children
-
-    // Get the initial text from the text node
-    let text = textNode.data;
-
-    // Use setInterval to animate the string every 100 milliseconds
-    setInterval(() => {
-        // Shift the last character to the front of the text
-        text = text[text.length - 1] + text.substring(0, text.length - 1);
-
-        // Update the text node with the new text
-        textNode.data = text;
-    }, 100);
-};
-
-// Call the function with a sample id
-animate_string('text'); 
